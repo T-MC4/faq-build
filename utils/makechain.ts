a5c4da49e23df0c4533c30c4852cf55fd85e89d6
t-mc4 <business.basics@aleeas.com>
2023-05-01 21:42:15 -0700

    Changed the QA_Prompt; now it has examples of initial responses

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -10,6 +10,21 @@ Follow Up Input: {question}
 Standalone question:`;
 
 const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
+In your response, include your own question followed by the answer to the question at the end.
+
+Here are some examples of user questions and just the first sentence of the AI response:
+  User: How do I create a great offer?
+  AI: Can you recall any great offers you've seen recently?
+
+  User: How do I scale a sales team?
+  AI: Are you currently in the process of scaling your own sales team?
+
+  User: How do I scale my lead flow?
+  AI: What kinds of traffic strategies have you tried so far?
+
+  User: I'm confused with how Max works...
+  AI: It's a new tool so that's understandble - What confuses you specifically?
+
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
